a09b3a0df4315cba4363fb8af214f0944431de51
R45HT1K15 <loverbulochek@yandex.ru>
2022-11-29 21:12:45 +0500

    route

--- controllers/vnukControllers.js
+++ controllers/vnukControllers.js
@@ -4,9 +4,18 @@ const Vnukogram = require('../views/Vnukogram');
 const Vnukolikes = require('../views/Vnukolikes');
 const Vnukoprofile = require('../views/Vnukoprofile');
 
-exports.vnukogram = (req, res) => {
-    render(Vnukogram, {}, res);
+const { Pictures } = require('../db/models');
+
+exports.vnukogram = async (req, res) => {
+    try {
+        const pictures = await Pictures.findAll({ order: [['id', 'DESC']]})
+        console.log('pictures---------------', pictures)
+        render(Vnukogram, { pictures }, res);
+    } catch (error) {
+        console.log('\x1b[31m', 'SignUp Error', error);
+    }
 };
+
 
 exports.vnukolikes = (req, res) => {
     render(Vnukolikes, {}, res);
