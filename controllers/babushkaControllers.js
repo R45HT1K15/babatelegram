115254cca99712b2699fadbf22ae908ce116668f
Valeria Safonova <strobagatova@yandex.ru>
2022-11-30 11:55:12 +0300

    visual add Like

--- controllers/babushkaControllers.js
+++ controllers/babushkaControllers.js
@@ -8,12 +8,23 @@ const BabushkaProfile = require('../views/BabushkaProfile');
 const { Picture, Grandparent, Like } = require('../db/models');
 
 exports.babushkagram = async (req, res) => {
+  const grandparent_id = req.session.user.id;
   try {
     const pictures = await Picture.findAll({ include: Grandparent, raw: true });
+    const likeOfUser = await Like.findAll({
+      attributes: ['picture_id'],
+      where: { grandparent_id },
+      raw: true,
+    });
+    const arrOfPicturesWhichUserLike = [];
+    likeOfUser.forEach((el) =>
+      arrOfPicturesWhichUserLike.push(el['picture_id'])
+    );
+
+    // console.log('pictures------------------', pictures);
 
     const { user } = req.session;
-    // console.log('pictures------------------', pictures[2])
-    render(Babushkagram, { pictures, user }, res);
+    render(Babushkagram, { pictures, user, arrOfPicturesWhichUserLike }, res);
   } catch (error) {
     console.log('\x1b[31m', 'Error', error);
   }
--- public/js/babushkaLike.js
+++ public/js/babushkaLike.js
@@ -4,21 +4,25 @@ document.addEventListener('click', async (event) => {
     const button = event.target.closest('button');
     const pictureId = event.target.dataset.pictureid;
     event.preventDefault();
-    const response = await fetch('/babushkagram/like', {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({ pictureId }),
-    });
-    const { created } = await response.json();
-    if (created) {
-      const newLike = `<svg data-like="like" data-pictureid=${pictureId} xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor"
-        class="bi bi-heart-fill" viewBox="0 0 16 16">
-        <path fill-rule="evenodd"
-          d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-      </svg> `;
-      button.innerHTML = newLike;
+    try {
+      const response = await fetch('/babushkagram/like', {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ pictureId }),
+      });
+      const { created } = await response.json();
+      if (created) {
+        const newLike = `<svg data-like="like" data-pictureid=${pictureId} xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor"
+          class="bi bi-heart-fill" viewBox="0 0 16 16">
+          <path fill-rule="evenodd"
+            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+        </svg> `;
+        button.innerHTML = newLike;
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 });
