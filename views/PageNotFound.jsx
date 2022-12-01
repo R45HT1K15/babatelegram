5b45adcec9cc85eafacfd8433ee4f9a89ec1c86e
TSvM777 <gaga123654@mail.ru>
2022-12-01 12:38:52 +0500

    error

---
+++ views/PageNotFound.jsx
@@ -0,0 +1,21 @@
+const React = require('react');
+const Layout = require('./Layout');
+
+module.exports = function PageNotFound({}) {
+return (
+<Layout>
+  <div>
+    <h2>Подробнее о сайте</h2>
+    <div className='mainPage'>
+      <div>
+        что-то о сайте
+      </div>
+      <div className='authBtns'>
+        <a href="/signup"><button className="btnAuth">Регистрация</button></a>
+        <a href="/signin"><button className="btnAuth">Войти</button></a>
+      </div>
+    </div>
+  </div>
+</Layout>
+);
+}
