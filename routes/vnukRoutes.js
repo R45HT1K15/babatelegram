98ba6eadf384c519487049fbef0267895ee704c7
Valeria Safonova <strobagatova@yandex.ru>
2022-12-02 16:33:11 +0300

    all

--- routes/vnukRoutes.js
+++ routes/vnukRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const route = express.Router();
 
 const {
-    vnukogram,
-    vnukolikes,
-    vnukoprofile,
-    addBabushka,
-    deleteBabushka
+  vnukogram,
+  vnukolikes,
+  vnukoprofile,
+  addBabushka,
+  deleteBabushka,
+  babushkaLike,
 } = require('../controllers/vnukControllers');
 
 route.get('/', vnukogram);
@@ -18,5 +19,6 @@ route.get('/profile', vnukoprofile);
 route.post('/profile', addBabushka);
 
 route.delete('/babushka', deleteBabushka);
+route.post('/babushkalike', babushkaLike);
 
 module.exports = route;
